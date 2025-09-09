85b4b7c025f9569193f2a35f2b05ab4a31ad212f
MennaFathi <mnamohamed098@gmail.com>
2025-09-09 20:52:54 +0200

    edit constant

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff2B475E);
+const kColorFriend = Color(0xFF68B8DA);
 const kLogo = 'assets/images/scholar.png';
 const kMessagesCollections = 'messages';
 const kMessage = 'message';
