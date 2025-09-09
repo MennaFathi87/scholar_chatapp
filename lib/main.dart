258b0c33ffa55672fb9c9f60f91a9fcb5c3e5ac8
MennaFathi <mnamohamed098@gmail.com>
2025-09-09 20:45:48 +0200

    add main

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:scholarchat/firebase_options.dart';
+import 'package:scholarchat/pages/Chat_page.dart';
+import 'package:scholarchat/pages/Register_page.dart';
+import 'package:scholarchat/pages/Sign_In.dart';
+
+void main() async {
+  //Firebase initialized
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  //to check Firebase initialized
+  print('Doneee....., Firebase initialized');
+  runApp(const ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        SignIn.id: (context) => SignIn(),
+        RegisterPage.id: (context) => RegisterPage(),
+        ChatPage.id: (context) => ChatPage()
+      },
+      home: SignIn(),
+    );
+  }
+}
